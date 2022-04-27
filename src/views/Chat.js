4e6a4eea94e4b707d1d8fe4db8eb738f202a8e32
Mauricio Silva <mauricio.souza@codificar.com.br>
2022-04-27 15:32:27 -0300

    adicionando oi

--- src/views/Chat.js
+++ src/views/Chat.js
@@ -7,7 +7,7 @@ export default class Chat extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            text: '',
+            text: 'oi',
             mensagens: [],
         }
     }
